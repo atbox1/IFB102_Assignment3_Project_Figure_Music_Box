29c387433b36191f8435450916323df6007dfff5
atbox1 <atbox@gmail.com>
2024-05-21 21:24:20 +1000

    Impliemented full Slide button functionalit,
    fixed bugs in LED & Buzzer class.
    
    Added Use of threading.

--- test-rpigpio.py
+++ test-rpigpio.py
@@ -2,10 +2,12 @@ import sys
 sys.path.insert(1, '/home/atbox/Documents/IFB102_Assignment3_Project/buzzer_tunes/')
 import RPi.GPIO as GPIO
 import random
+import threading
 from gpiozero import Button 
 from time import sleep
 #Importing @... from foldar
 from buzzer_tunes import PlayMidi, SongsMidi, buzzer_music, machine
+
 
 #Confiration
 GPIO.setmode(GPIO.BCM)
@@ -24,6 +26,11 @@ GPIO.setwarnings(False)
 #LEDs
 #A: R 14, O 15, G 18, W 5
 #B R 16, O 12
+
+PressButton = Button(26)
+
+
+
 
 class DriveMotor:
     #Low High is backwards
@@ -58,20 +65,24 @@ class DriveMotor:
 
 class Buzzer:
     def __init__(self, song):
+        VariableA = buzzer_music
         #self.Song = Song
         self.song = song
         self.mySong = buzzer_music.music(self.song, pins=[machine.Pin(22)])
 
-    # def Play(self):
-    #     # PlayMidi.RunSong(SongsMidi.JingleBells)
-    #
-    #
-    def Stop(self):
-        #PlayMidi.RunSong(self.Stop())
-        #machine.Stop(22)
-        #buzzer_music.music.stop()
 
-        pass
+
+    # def Play(self):
+    #     PlayMidi.RunSong(SongsMidi.JingleBells)
+
+    def Play(self):
+        #while PressButton.value == 1:
+        while PressButton.value == 1:
+            sleep(0.04)
+            self.mySong.tick()
+
+
+
 
 
 
@@ -81,6 +92,7 @@ class Patterns:
     #     self.pattern = pattern
     def ModifyList(self, i = 0, ii = 0, iii = 0, iv = 0, v = 0, vi = 0, vii = 0, viii = 0, time = 0.1):
 
+
         InputList = [i, ii, iii, iv, v, vi, vii, viii]
         for LED in InputList:
             if LED != 0:
@@ -88,8 +100,6 @@ class Patterns:
                 GPIO.output(LED, GPIO.HIGH)
                 sleep(time)
                 GPIO.output(LED, GPIO.LOW)
-
-
 
 class LEDsystem:
     def __init__(self):
@@ -121,121 +131,70 @@ class LEDsystem:
         PatternObject = Patterns()
         delayList = [0.3, 0.4, 0.08, 1]
 
+        while PressButton.value == 1:
+            match pattern:
+                case 1:
+                        PatternObject.ModifyList(14, 18, 23, 15, delayList[0])
+                        sleep(delayList[3])
+                        PatternObject.ModifyList(16, 12, 20, 21, delayList[0])
+                case 2:
+                        PatternObject.ModifyList(14, 16, time = delayList[1])
+                        PatternObject.ModifyList(18, 12, time = delayList[1])
+                        PatternObject.ModifyList(23, 20, time = delayList[1])
+                        PatternObject.ModifyList(15, 21, time = delayList[1])
 
-        #while True:
-        match pattern:
-            case 1:
-                #while True:
-                #Sides Style
-                    PatternObject.ModifyList(14, 18, 23, 15, delayList[0])
-                    sleep(delayList[3])
-                    PatternObject.ModifyList(16, 12, 20, 21, delayList[0])
-            case 2:
-                #Alternating between Colours
-                #while True:
-                    PatternObject.ModifyList(14, 16, time = delayList[1])
-                    PatternObject.ModifyList(18, 12, time = delayList[1])
-                    PatternObject.ModifyList(23, 20, time = delayList[1])
-                    PatternObject.ModifyList(15, 21, time = delayList[1])
+                case 3:
+                        PatternObject.ModifyList(14, 18, 23, 15
+                        , 21, 20, 12, 16, time=0.08)
+                case 4:
+                        PatternObject.ModifyList(14, 21, time = delayList[1])
+                        sleep(delayList[5])
+                        PatternObject.ModifyList(15, 16, time = delayList[1])
+                        sleep(delayList[5])
+                        PatternObject.ModifyList(18, 20, time = delayList[1])
+                        sleep(delayList[5])
+                        PatternObject.ModifyList(23, 12, time = delayList[1])
 
-            case 3:
-                #Rotate through LEDs
-                #while True:
+                case 5:
+                        random.shuffle(self.LEDs)
+                        print(self.LEDs)
+                        PatternObject.ModifyList(self.LEDs[0], self.LEDs[1], self.LEDs[2], self.LEDs[3], self.LEDs[4], self.LEDs[5], self.LEDs[6], self.LEDs[7], time=delayList[1])
+                case _:
                     PatternObject.ModifyList(14, 18, 23, 15
-                    , 21, 20, 12, 16, time=0.08)
-            case 4:
-                #Checkers
-                #while True:
-                    PatternObject.ModifyList(14, 21, time = delayList[1])
-                    sleep(delayList[5])
-                    PatternObject.ModifyList(15, 16, time = delayList[1])
-                    sleep(delayList[5])
-                    PatternObject.ModifyList(18, 20, time = delayList[1])
-                    sleep(delayList[5])
-                    PatternObject.ModifyList(23, 12, time = delayList[1])
-
-            case 5:
-                #Random Order
-                #while True:
-                    random.shuffle(self.LEDs)
-                    print(self.LEDs)
-                    PatternObject.ModifyList(self.LEDs[0], self.LEDs[1], self.LEDs[2], self.LEDs[3], self.LEDs[4], self.LEDs[5], self.LEDs[6], self.LEDs[7], time=delayList[1])
-            case _:
-                #while True:
-                PatternObject.ModifyList(14, 18, 23, 15
-                    , 16, 12, 20, 21, time=delayList[0])
-                    #pass
-
-
-
-# class Button:
-#     def __init__(self, pin):
-#         self.pin = pin
-#         GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#
+                        , 16, 12, 20, 21, time=delayList[0])
 
 class Main:
-    button = Button(26)
-    MotorObject = DriveMotor(6, 5, 11)
-    LEDObject = LEDsystem()
 
 
-    # MotorObject = DriveMotor(6, 5, 11)
-    #PressButton = Button(26)
-    # LEDObject = LEDsystem()
-    # GPIO.setup(26, GPIO.IN)
-    # SlideButton = GPIO.input(26)
-    #Test = Patterns(1)
-
-    #MotorObject.Stop()
-    BuzzerObject = Buzzer(SongsMidi.SimpleRhythme)
-    while True:
-
-        if button.is_pressed:
+    def __init__(self):
+        self.MotorObject = DriveMotor(6, 5, 11)
+        self.BuzzerObject = Buzzer(SongsMidi.JingleBells)
+        self.LEDObject = LEDsystem()
+        self.RunInstance()
 
 
-            BuzzerObject.mySong.tick()
-            sleep(0.04)
-            LEDObject.PatternMaker(1)
-        else:
-            BuzzerObject.mySong.stop()
-            print("0")
-            pass
+    def RunInstance(self):
+        while True:
 
-    # if button.is_pressed:
-    #     while True:
-    #         BuzzerObject.mySong.tick()
-    #         sleep(0.04)
-    # else:
-    #     print("E")
+            if PressButton.value == 1:
 
-    # while True:
+                LEDThread = threading.Thread(target=self.LEDObject.PatternMaker, args=(1,))
+                BuzzerThread = threading.Thread(target=self.BuzzerObject.Play)
+                self.MotorObject.Drive('B', 25)
 
-    #PressButton.wait_for_press()
+                LEDThread.start()
+                BuzzerThread.start()
 
-    # while True:
-    #     MotorObject.Drive('F', 25 )
-    # #BuzzerObject.Play()
-    # # sleep(0.1)
-    # #LEDObject.CoolLookingThings
-    # #MotorObject.Stop()
-    # #sleep(0.1)
-    # #LEDObject.PowerStates[0]
-    # #LEDObject.PatternMaker(8)
-    #     PatternObject = Patterns()
-    #     delayList = [0.3, 0.4, 0.08, 1]
-    #
-    #     PatternObject.ModifyList(14, 18, 23, 15
-    #                              , 16, 12, 20, 21, time=delayList[0])
-    #     break
-    #
+                LEDThread.join()
+                BuzzerThread.join()
 
-    #sleep(0.6)
-    #
-    # PressButton.wait_for_press()
-    #
-    # MotorObject.Stop()
-    # LEDObject.PowerStates(0)
+                sleep(0.5)
+
+            else:
+                sleep(0.5)
+                self.MotorObject.Stop()
+
+                continue
 
 
 
@@ -245,16 +204,7 @@ class Main:
 
 
 
-# PressButton.wait_for_press()
-# MotorObject.Stop()
-# sleep(400)
-
-#MotorObject.Stop()
-    #LEDObject.PowerStates(0)
-    #GPIO.cleanup()
 
 
-
-
-RunInstance = Main()
+MainInstance = Main()
 
